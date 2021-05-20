43bfb9000f1eb0f202c0cbc86d84183a632e029d
thomasabishop <tactonbishop@gmail.com>
2021-05-20 20:03:23 +0100

    Set up Salaried work section

---
+++ src/data/salaried.js
@@ -0,0 +1,16 @@
+const salaried = [
+  {
+    client: 'University of Sussex',
+    description:
+      "Hired by the University of Sussex's Digital Humanities Lab to customise the Making African Connections Digital Archive, an AHRC-funded research project. Involved frontend alterations and the creation of several backend processes designed to assist the researchers in managing content. Primarily worked with PHP, MySQL, and JavaScript.",
+    url: 'https://makingafricanconnections.org/s/archive/page/index',
+  },
+  {
+    client: 'Nuance Communications',
+    description:
+      'Nuance are one of the leading developers of AI-driven speech-recognition technology. Worked with Nuance to produce landing pages and other web content for marketing campaigns. Involved refactoring the existing frontend code and using JavaScript to automate page behaviour on the basis of date/time and tracking codes contained within the inbound marketing URLs.',
+    url: 'https://www.nuance.com/en-gb/index.html',
+  },
+];
+
+export default salaried;

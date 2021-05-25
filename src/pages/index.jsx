2fe67e477df4469e9007270b15f3abbf69636220
thomasabishop <tactonbishop@gmail.com>
2021-05-25 09:23:24 +0100

    Hide wakatime components from build

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import Seo from '../components/seo';
 import Welcome from '../components/Welcome';
 import Projects from '../components/Projects';
 import PostPreview from '../components/PostPreview';
-import CodingGraphs from '../components/CodingGraphs';
+//import CodingGraphs from '../components/CodingGraphs';
 import MainTemplate from '../templates/MainTemplate';
 import Freelance from '../components/Freelance';
 import ContactForm from '../components/ContactForm';

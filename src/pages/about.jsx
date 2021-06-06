9b80cb48b6260719bcab668b69e92c7776303b62
thomasabishop <tactonbishop@gmail.com>
2021-06-06 22:52:53 +0100

    Correct typos About page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import MainTemplate from '../templates/MainTemplate';
-import { Container } from '../styles/Container';
-import Freelance from '../components/Freelance';
-import Salaried from '../components/Salaried';
+import React from "react"
+import MainTemplate from "../templates/MainTemplate"
+import { Container } from "../styles/Container"
+import Freelance from "../components/Freelance"
+import Salaried from "../components/Salaried"
 export default function Posts() {
   return (
     <MainTemplate>
@@ -10,18 +10,21 @@ export default function Posts() {
         <h1>About</h1>
         <p>
           I was born in 1988 and grew up in south/central London. I was educated
-          at <b>Wimbledon College</b> (2001-2006) and studied philosophy at the{' '}
-          <b>University of Warwick</b> (2006-2009). Later I trained as a primary
-          school teacher, completing my Postgraduate Certificate of Education
-          and <b>Middlesex University</b> (2011-2012). I taught in schools
-          throughout south London for several years and then taught myself how
-          to code and switched to web development. I have worked as a developer
-          in both a salaried and freelance capacity. I now work exclusively as a
-          frontend software engineer in Scotland.
+          at <b>Wimbledon College</b> (2001-2006) and studied philosophy at the{" "}
+          <b>University of Warwick</b> (2006-2009).
+        </p>
+        <p>
+          Later I trained as a primary school teacher, completing my
+          Postgraduate Certificate of Education at <b>Middlesex University</b>{" "}
+          (2011-2012). I taught in schools throughout south London for several
+          years and then taught myself how to code and switched to web
+          development. I have worked as a developer in both a salaried and
+          freelance capacity. I now work exclusively as a frontend software
+          engineer in Scotland.
         </p>
         <p>
           In my free time, outside of computing and improving my technical
-          skills and knowledge, I enjoy reading popular science, philosophy and
+          knowledge, I enjoy reading popular science, philosophy and
           science-fiction; learning languages; listening to podcasts and
           electronic/heavy metal music; spending time with Staffies; lifting
           weights and exploring Scotland.
@@ -31,5 +34,5 @@ export default function Posts() {
 
       <Freelance />
     </MainTemplate>
-  );
+  )
 }

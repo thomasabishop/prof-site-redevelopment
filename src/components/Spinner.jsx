f0c1a6c500e0303b8906ebcf949cef843871e537
thomasabishop <tactonbishop@gmail.com>
2021-07-18 19:40:43 +0100

    Change spinner from circular to square.

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Loader from 'react-spinners/MoonLoader';
+import Loader from 'react-spinners/ScaleLoader';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`

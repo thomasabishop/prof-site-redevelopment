ff140c0e19043a81dbf4dd495c5503892bd04147
thomasabishop <tactonbishop@gmail.com>
2021-10-16 16:12:29 +0100

    Rephrase Arria description.

--- src/data/salaried.js
+++ src/data/salaried.js
@@ -1,19 +1,19 @@
 const salaried = [
   {
-    jobTitle: 'Software Engineer',
-    dates: '2021 - present',
-    linkUrl: 'https://www.arria.com/',
-    linkText: 'Arria Natural Language Generation',
+    jobTitle: "Software Engineer",
+    dates: "2021 - present",
+    linkUrl: "https://en.wikipedia.org/wiki/Arria_NLG",
+    linkText: "Arria Natural Language Generation",
     description:
-      "I work as part of the frontend team on NLG Studio, Arria's flagship software. NLG Studio replicates the human process of expertly analyzing and communicating data insights by dynamically turning structured data into written or spoken narrative at scale and at machine speed. I primarily work with TypeScript and Angular.",
+      "I work as part of the frontend team on NLG Studio, Arria's flagship software. Arria NLG Studio converts complex structured data into natural language narratives and is primarily used as an API for automated technical reporting. I work with TypeScript and Angular.",
   },
   {
-    jobTitle: 'Assistant Web Developer',
-    dates: '2019 - 2021',
-    linkUrl: 'http://www.bdcdesign.com/',
-    linkText: 'Blueprint Design Company',
+    jobTitle: "Assistant Web Developer",
+    dates: "2019 - 2021",
+    linkUrl: "http://www.bdcdesign.com/",
+    linkText: "Blueprint Design Company",
     description:
-      'Whilst at BDC I worked as the lead frontend developer using vanilla JavaScript and React, along with other standard web technologies and tooling. I worked closely with graphic and UX designers to produce single page applications primarily for large global health technology providers, most notably Philips.',
+      "Whilst at BDC I worked as the lead frontend developer using vanilla JavaScript and React, along with other standard web technologies and tooling. I worked closely with graphic and UX designers to produce single page applications primarily for large global health technology providers, most notably Philips.",
   },
 ];
 

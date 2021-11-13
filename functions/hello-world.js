f982434a02a8809b85cf04a30f1468d74916e564
thomasabishop <tactonbishop@gmail.com>
2021-11-13 15:42:14 +0000

    Delete old files.

--- functions/hello-world.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async function () {
-  return {
-    statusCode: 200,
-    body: 'Hello world!',
-  };
-};
-
-// https://www.joshwcomeau.com/gatsby/using-netlify-functions-with-gatsby/
